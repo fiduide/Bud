d074ac489779dcfbf46b5ea917a1d17b5a217650
Dorian <dorian161100@hotmail.fr>
2021-04-15 08:25:09 +0200

    affinement des règles pendu

--- function/pendu.js
+++ function/pendu.js
@@ -19,7 +19,7 @@ function pendu(message, connection, client) {
             }
             if (results[0] == null) {
                 message.channel.send("***Initialisation de la partie... Veuillez patienter.***");
-                message.channel.send("***Les règles sont simple : \n- Au bout de 10 fautes, la partie s'arrête.\n- Vous pouvez essayer des mots directement.\n- Pour jouer c'est simple, tapé !pendu (lettre/mot)***");
+                message.channel.send("***Les règles sont simple : \n- Tout le monde peut y participer.\n- Au bout de 10 fautes, la partie s'arrête.\n- Vous pouvez essayer des mots directement.\n- Pour jouer c'est simple, tapé !pendu (lettre/mot)***");
                 realMot = tabMots[indexMotSearch];
 
                 let buff = "";
